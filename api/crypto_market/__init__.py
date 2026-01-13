8c2aba1db2cb015c77086ee53d13602c6fc1a8ad
Jeon <youngbeen@Jeons-MacBook-Pro.local>
2026-01-14 00:06:28 +0900

    DELETE api/ folder - Render should use flask-backend/

--- api/crypto_market/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import Candle
-from .indicators import ema, atr, wick_ratio
-from .market_gate import evaluate_market_gate, run_market_gate_sync, MarketGateResult
--- api/crypto_market/lead_lag/__init__.py
+++
@@ -1,2 +0,0 @@
-from .granger import GrangerResult, granger_causality_test, find_granger_causal_indicators
-from .data_fetcher import DataSource, MARKET_SOURCES, fetch_yfinance_data, fetch_all_data
